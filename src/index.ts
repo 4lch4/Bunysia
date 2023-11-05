35c438e6ef250877e893b0d6df5c7e975d9b9dfa
Devin W. Leaman <git@4lch4.email>
2023-11-05 14:08:57 -0600

    chore: final cleanup before deployment

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,16 @@
-import { prefixedHealthCheckRoutes, printRoutes } from '@4lch4/backpack/elysia'
+import { logger } from '@4lch4/backpack'
+import { HealthCheckRoutes, printRoutes } from '@4lch4/backpack/elysia'
 import { cron } from '@elysiajs/cron'
 import { swagger } from '@elysiajs/swagger'
 import { logger as logysia } from '@grotto/logysia'
 import { Elysia } from 'elysia'
 import { SlackRoute } from './api'
 import { runJob } from './batch'
-// import { logger } from './lib'
-import { logger } from '@4lch4/backpack'
 
 export const app = new Elysia()
   .use(logysia())
   .use(swagger())
-  .use(prefixedHealthCheckRoutes('/status'))
+  .use(HealthCheckRoutes('/status'))
   .use(SlackRoute)
   .use(
     cron({
@@ -22,14 +21,12 @@ export const app = new Elysia()
         await runJob()
       },
     }),
-  )
+  ).listen(process.env.APP_PORT || 4242)
 
 // Only output the routes & success logs if we're not in a test environment.
 if (!Bun.main.includes('bunysia/test')) {
-  printRoutes(app.routes)
+  printRoutes(app.routes, { multiMethodRows: true })
 
   logger.success('The server has come online!')
   logger.success(`Listening on ${app.server?.hostname}:${app.server?.port}...`)
 }
-
-app.listen(process.env.APP_PORT || 4242)
